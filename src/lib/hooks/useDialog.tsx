c49749dab43451ee555dde957236f9d95dcbf6ec
Guo <hashencode@gmail.com>
2024-09-19 15:24:24 +0800

    feat: 修复ts警告

--- src/lib/hooks/useDialog.tsx
+++ src/lib/hooks/useDialog.tsx
@@ -1,10 +1,11 @@
 import { createDialog, destroyDialog, destroyAllDialog } from '@/lib/store/libSlice';
 import { useDispatch } from 'react-redux';
 import { IDialogQueueItem } from '@/lib/_types/.store';
+import { IDialogProps } from '@/lib/_types/.components';
 
 export default function useDialog() {
     const dispatch = useDispatch();
-    const create = (data: IDialogQueueItem) => dispatch(createDialog(data));
+    const create = (data: IDialogProps) => dispatch(createDialog(data));
     const destroy = (data: IDialogQueueItem['id']) => dispatch(destroyDialog(data));
     const destroyAll = () => dispatch(destroyAllDialog());
 
--- src/lib/hooks/useToast.tsx
+++ src/lib/hooks/useToast.tsx
@@ -1,10 +1,11 @@
 import { createToast, destroyToast, destroyAllToast } from '@/lib/store/libSlice';
 import { useDispatch } from 'react-redux';
 import { IToastQueueItem } from '@/lib/_types/.store';
+import { IToastProps } from '@/lib/_types/.components';
 
 export default function useToast() {
     const dispatch = useDispatch();
-    const create = (data: IToastQueueItem) => dispatch(createToast(data));
+    const create = (data: IToastProps) => dispatch(createToast(data));
     const destroy = (data: IToastQueueItem['id']) => dispatch(destroyToast(data));
     const destroyAll = () => dispatch(destroyAllToast());
 
