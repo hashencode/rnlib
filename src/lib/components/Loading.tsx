830f297beeac51c51090bab23c2ac2cf7f6a7bb4
Guo <hashencode@gmail.com>
2024-09-19 15:05:50 +0800

    feat: Loading

--- src/lib/components/Loading.tsx
+++ src/lib/components/Loading.tsx
@@ -1,37 +1,30 @@
 import { useEffect } from 'react';
 import { COLOR } from '@/lib/scripts/const';
-import Icon from './Icon';
-import { StyleSheet } from 'react-native';
 import { SIZE } from '@/lib/scripts/const';
 import Animated, { cancelAnimation, useAnimatedStyle, useSharedValue, Easing, withRepeat, withTiming } from 'react-native-reanimated';
-import { mergeElement } from '@/lib/scripts/utils';
 import { ILoadingProps } from '@/lib/_types/.components';
+import Icon from '@/lib/components/Icon';
 
 export default function Loading(props: ILoadingProps) {
-    const { icon = <Icon name="loader"></Icon>, color = COLOR.icon_touchable, size = SIZE.icon_md, style } = props;
+    const { color = COLOR.icon_touchable, size = SIZE.icon_md, style } = props;
 
     const angle = useSharedValue(0);
 
     useEffect(() => {
-        angle.value = withRepeat(withTiming(1, { duration: 1800, easing: Easing.linear }), -1);
-        return () => {
-            cancelAnimation(angle);
-        };
+        cancelAnimation(angle);
+        angle.value = 0;
+        angle.value = withRepeat(withTiming(365, { duration: 1000, easing: Easing.linear }), -1, false);
     }, []);
 
     const animatedStyle = useAnimatedStyle(() => {
         return {
-            transform: [{ rotate: `${angle.value * 360}deg` }],
+            transform: [{ rotate: `${angle.value}deg` }],
         };
     });
 
-    return <Animated.View style={[styles.root, animatedStyle, style]}>{mergeElement(icon, { size, color })}</Animated.View>;
+    return (
+        <Animated.View style={[animatedStyle, style]}>
+            <Icon name="loader-circle" color={color} size={size}></Icon>
+        </Animated.View>
+    );
 }
-
-const styles = StyleSheet.create({
-    root: {
-        alignItems: 'center',
-        display: 'flex',
-        justifyContent: 'center',
-    },
-});
