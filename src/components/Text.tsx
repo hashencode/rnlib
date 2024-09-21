e7c0e7781fed5db52c19aa7390ea294431ae67fa
HashPuppy <hashencode@gmail.com>
2024-09-21 12:52:53 +0800

    feat: Text区分系统行高

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,12 +1,14 @@
-import { useMemo } from 'react';
-import { StyleSheet, Text as TextOrigin } from 'react-native';
+import { Text as TextOrigin } from 'react-native';
 import { TextStyle } from 'react-native/Libraries/StyleSheet/StyleSheetTypes';
 import { COLOR, SIZE } from '../scripts/const';
 import { ITextProps } from '../_types/components';
 import useStyle from '../hooks/useStyle';
+import { isAndroid } from '../scripts/utils';
 
 export default function Text(props: ITextProps) {
     const { style, weight = 'normal', size = SIZE.font_basic, color = COLOR.text_title, children, ...rest } = props;
+
+    const platformStyle = isAndroid() ? { lineHeight: 1.3 * (style?.fontSize || size) } : {};
 
     const rootStyle = useStyle<TextStyle>({
         defaultStyle: [
@@ -15,6 +17,7 @@ export default function Text(props: ITextProps) {
                 color,
                 fontWeight: weight,
             },
+            platformStyle,
         ],
         extraStyle: [style],
     });
