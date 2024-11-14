f14d9aee21d2b7a4c1372b171b91a8db90fb5ea4
Guo <hashencode@gmail.com>
2024-11-14 11:41:55 +0800

    feat: 修改imageX组件底层依赖

--- src/components/ImageX.tsx
+++ src/components/ImageX.tsx
@@ -1,25 +1,17 @@
-import { Image as ImageOrigin, ImageResizeMode, ImageStyle, StyleProp } from 'react-native';
+import { ImageStyle, StyleProp } from 'react-native';
 import { SIZE } from '../scripts/const';
-import { ImageProps as ImageOriginProps } from 'react-native/Libraries/Image/Image';
+import TurboImage, { TurboImageProps } from 'react-native-turbo-image';
 
-export interface IImageXProps extends ImageOriginProps {
+export interface IImageXProps extends Omit<TurboImageProps, 'style'> {
     height?: number; // 高度
     radius?: number; // 圆角
-    resizeMode?: ImageResizeMode; // 图片裁剪模式
     size?: number; // 宽高尺寸
     style?: StyleProp<ImageStyle>; // 样式
     width?: number; // 宽度
 }
 
 export default function ImageX(props: IImageXProps) {
-    const {
-        defaultSource = require('../assets/images/imagePlaceholder.jpg'),
-        height = props.size,
-        radius = SIZE.radius_md,
-        style = {},
-        width = props.size,
-        ...rest
-    } = props;
+    const { height = props.size, radius = SIZE.radius_md, style = {}, width = props.size, ...rest } = props;
 
-    return <ImageOrigin defaultSource={defaultSource} width={width} height={height} borderRadius={radius} {...rest} style={style} />;
+    return <TurboImage {...rest} style={[{ width, height, borderRadius: radius }, style]} />;
 }
