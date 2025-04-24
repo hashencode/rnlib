2083d7deb26c8d057295bfd3207c6e7b83daa928
Guo <hashencode@gmail.com>
2025-04-24 11:45:07 +0800

    fix: 修复ImageX组件在安卓机型下无法自动设置高度的问题

--- src/components/ImageX.tsx
+++ src/components/ImageX.tsx
@@ -1,12 +1,11 @@
-import { DimensionValue, ImageStyle, LayoutChangeEvent, StyleProp } from 'react-native';
+import { DimensionValue, Image, ImageRequireSource, ImageStyle, LayoutChangeEvent, StyleProp } from 'react-native';
 import { useRef, useState } from 'react';
-import _ from 'lodash';
-import FastImage, { FastImageProps } from '@d11/react-native-fast-image';
+import { isUndefined } from 'lodash';
+import FastImage, { FastImageProps, Source } from '@d11/react-native-fast-image';
 
 export interface IImageXProps extends Omit<FastImageProps, 'style'> {
-    height?: number; // 高度
+    height?: DimensionValue; // 高度
     radius?: number; // 圆角
-    size?: DimensionValue; // 宽高尺寸
     style?: StyleProp<ImageStyle>; // 样式
     width?: DimensionValue; // 宽度
     onError?: () => void;
@@ -15,33 +14,35 @@ export interface IImageXProps extends Omit<FastImageProps, 'style'> {
 }
 
 export default function ImageX(props: IImageXProps) {
-    const { height, radius = 0, style = {}, width = props.size, onError, onLayout, ...rest } = props;
+    const { height, radius = 0, style = {}, width, source, onError, onLayout, ...rest } = props;
 
     const imageWidth = useRef(0);
-    const [innerHeight, setInnerHeight] = useState(props.size || height);
+    const [innerHeight, setInnerHeight] = useState(height);
+    const [sourceValue, setSourceValue] = useState<Source | ImageRequireSource>();
 
     const handleLayout = (event: LayoutChangeEvent) => {
         imageWidth.current = event.nativeEvent.layout.width;
         onLayout?.(event);
-    };
 
-    const handleLoad = (event: any) => {
-        const { width: originalWidth, height: originalHeight } = event.nativeEvent;
-        if (originalWidth && originalHeight) {
-            const aspectRatio = originalHeight / originalWidth;
-            if (_.isUndefined(innerHeight)) {
-                setInnerHeight(imageWidth.current * aspectRatio);
-            }
+        if (isUndefined(height) && (source as Source)?.uri) {
+            Image.getSize((source as Source)?.uri as string, (width: number, height: number) => {
+                const aspectRatio = height / width;
+                if (isUndefined(innerHeight)) {
+                    setInnerHeight(imageWidth.current * aspectRatio);
+                }
+                setSourceValue(source);
+            });
         }
     };
 
     return (
         <FastImage
+            source={sourceValue}
             onLayout={handleLayout}
-            onLoad={handleLoad}
             onError={onError}
+            resizeMode={FastImage.resizeMode.stretch}
             {...rest}
-            style={[{ width, height: innerHeight, borderRadius: radius }, style as FastImageProps['style']]}
+            style={[{ width, height: innerHeight || 800, borderRadius: radius }, style as FastImageProps['style']]}
         />
     );
 }
