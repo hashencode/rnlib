3a4d6b44027bf8a3f842794461dcd7df7bd38cda
Guo <hashencode@gmail.com>
2025-04-28 10:54:18 +0800

    fix: 修复图片自动高度问题

--- src/components/ImageX.tsx
+++ src/components/ImageX.tsx
@@ -1,4 +1,4 @@
-import { DimensionValue, Image, ImageRequireSource, ImageStyle, LayoutChangeEvent, StyleProp } from 'react-native';
+import { DimensionValue, Image, ImageStyle, LayoutChangeEvent, StyleProp } from 'react-native';
 import { useRef, useState } from 'react';
 import { isUndefined } from 'lodash';
 import FastImage, { FastImageProps, Source } from '@d11/react-native-fast-image';
@@ -18,26 +18,24 @@ export default function ImageX(props: IImageXProps) {
 
     const imageWidth = useRef(0);
     const [innerHeight, setInnerHeight] = useState(height);
-    const [sourceValue, setSourceValue] = useState<Source | ImageRequireSource>();
 
     const handleLayout = (event: LayoutChangeEvent) => {
         imageWidth.current = event.nativeEvent.layout.width;
         onLayout?.(event);
 
         if (isUndefined(height) && (source as Source)?.uri) {
-            Image.getSize((source as Source)?.uri as string, (width: number, height: number) => {
-                const aspectRatio = height / width;
+            Image.getSize((source as Source)?.uri as string, (_width, _height) => {
+                const aspectRatio = _height / _width;
                 if (isUndefined(innerHeight)) {
                     setInnerHeight(imageWidth.current * aspectRatio);
                 }
-                setSourceValue(source);
             });
         }
     };
 
     return (
         <FastImage
-            source={sourceValue}
+            source={source}
             onLayout={handleLayout}
             onError={onError}
             resizeMode={FastImage.resizeMode.stretch}
